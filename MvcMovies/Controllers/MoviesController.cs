ed1c88e01bf44bdb938b2d6f0da971cd1f7c56ea
Raymond Yau <raymondccyau@gmail.com>
2019-01-04 14:07:55 +0000

    Completed Tutorial - all CRUD added

--- MvcMovies/Controllers/MoviesController.cs
+++ MvcMovies/Controllers/MoviesController.cs
@@ -70,7 +70,7 @@ namespace MvcMovies.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -102,7 +102,7 @@ namespace MvcMovies.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
